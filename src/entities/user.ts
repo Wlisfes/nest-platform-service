5b2909050d126156aea821ef32906e4d9ae638e4
妖雨纯 <876451336@qq.com>
2024-09-18 20:51:28 +0800

    ✨ feat(entities): 新增表结构定义规范

---
+++ src/entities/user.ts
@@ -0,0 +1,46 @@
+import { Entity, Column, Index } from 'typeorm'
+import { hashSync } from 'bcryptjs'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, Length, IsEmail, IsNumber, IsEnum, Min, Max } from 'class-validator'
+import { Type } from 'class-transformer'
+import { CommonEntier } from '@/utils/utils-typeorm'
+
+@Entity({ name: 'user', comment: '用户表' })
+export class UserEntier extends CommonEntier {
+    @ApiProperty({ description: 'UID', example: '2149446185344106496' })
+    @IsNotEmpty({ message: 'UID必填' })
+    @Column({ comment: '唯一UUID', length: 32, nullable: false })
+    @Index()
+    uid: string
+
+    @ApiProperty({ description: '昵称', example: '妖雨纯' })
+    @IsNotEmpty({ message: '昵称必填' })
+    @Length(2, 32, { message: '昵称必须保持2~32位' })
+    @Column({ comment: '昵称', length: 32, nullable: false })
+    nickname: string
+
+    @ApiProperty({ description: '头像' })
+    @IsNotEmpty({ message: '头像必填' })
+    @Length(4, 255, { message: '头像地址必须保持4~255位' })
+    @Column({ comment: '头像', nullable: false })
+    avatar: string
+
+    @ApiProperty({ description: '邮箱', example: 'limvcfast@gmail.com' })
+    @IsNotEmpty({ message: '邮箱必填' })
+    @IsEmail({}, { message: '邮箱格式错误' })
+    @Length(4, 32, { message: '邮箱必须保持4~64位' })
+    @Column({ comment: '邮箱', length: 64, nullable: false })
+    email: string
+
+    @ApiProperty({ description: '密码', example: 'MTIzNDU2' })
+    @IsNotEmpty({ message: '密码必填' })
+    @Length(6, 32, { message: '密码必须保持6~32位' })
+    @Column({
+        comment: '密码',
+        length: 128,
+        select: false,
+        nullable: false,
+        transformer: { from: value => value, to: value => hashSync(value) }
+    })
+    password: string
+}
