5fc787bbc9dced9bfc29dc6dd0c2aad9a2698bf6
妖雨纯 <876451336@qq.com>
2024-09-19 09:00:43 +0800

    ✨ feat(modules): 新增mysql连接服务

--- apps/web-auth-service/src/web-auth.module.ts
+++ apps/web-auth-service/src/web-auth.module.ts
@@ -1,8 +1,26 @@
-import { Module } from '@nestjs/common'
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
+import { APP_INTERCEPTOR, APP_FILTER, APP_GUARD } from '@nestjs/core'
+import { LoggerMiddleware } from '@/middleware/logger.middleware'
+import { UserAgentMiddleware } from '@/middleware/useragent.middleware'
+import { AuthGuard } from '@/guard/auth.guard'
+import { TransformInterceptor } from '@/interceptor/transform.interceptor'
+import { HttpExceptionFilter } from '@/filter/http-exception.filter'
+import { ConfigerModule } from '@/modules/configer.module'
+import { LoggerModule } from '@/modules/logger.module'
+import { ThrottlerModule } from '@/modules/throttler.module'
+import { DatabaseModule } from '@/modules/database.module'
 
 @Module({
-    imports: [],
+    imports: [LoggerModule.forRoot({ name: 'web-auth-service' }), ConfigerModule, ThrottlerModule, DatabaseModule],
     controllers: [],
-    providers: []
+    providers: [
+        { provide: APP_GUARD, useClass: AuthGuard },
+        { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+        { provide: APP_FILTER, useClass: HttpExceptionFilter }
+    ]
 })
-export class WebAuthModule {}
+export class WebAuthModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(UserAgentMiddleware, LoggerMiddleware).forRoutes('*')
+    }
+}
---
+++ src/modules/database.module.ts
@@ -0,0 +1,31 @@
+import { Module, Global } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as entities from '@/entities/instance'
+export const forEntities = [entities.UserEntier]
+
+@Global()
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    type: config.get('ORM_TYPE'),
+                    host: config.get('ORM_HOST'),
+                    port: config.get('ORM_PORT'),
+                    username: config.get('ORM_USERNAME'),
+                    password: config.get('ORM_PASSWORD'),
+                    database: config.get('ORM_DATABASE'),
+                    charset: config.get('ORM_CHARSET'),
+                    synchronize: true,
+                    entities: forEntities
+                } as TypeOrmModuleOptions
+            }
+        }),
+        TypeOrmModule.forFeature(forEntities)
+    ],
+    providers: [],
+    exports: []
+})
+export class DatabaseModule {}
