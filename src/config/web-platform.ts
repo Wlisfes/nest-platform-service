c6021da28ea79541dbb13e0277c324efa7afb84b
妖雨纯 <876451336@qq.com>
2024-09-27 14:22:12 +0800

    ✨ feat(web-system-service): 新增系统配置服务

--- src/config/web-platform.ts
+++ src/config/web-platform.ts
@@ -1,9 +1,19 @@
 export const WebAccountService = {
     namespace: 'web-account-service',
-    title: '基础授权服务',
-    siteTitle: '基础授权服务API文档',
-    description: 'Basic authorization API Documentation',
+    title: '账户中心服务',
+    siteTitle: '账户中心服务API文档',
+    description: 'Account Center Service API Documentation',
     version: '1.0.0',
     document: 'api-doc',
     port: 4080
+}
+
+export const WebSystemService = {
+    namespace: 'web-system-service',
+    title: '系统配置服务',
+    siteTitle: '系统配置服务API文档',
+    description: 'System Configuration Service API Documentation',
+    version: '1.0.0',
+    document: 'api-doc',
+    port: 4070
 }
