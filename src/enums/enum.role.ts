bfac08eede647dace1d3cbe01268686321347fa8
妖雨纯 <876451336@qq.com>
2024-10-22 15:14:38 +0800

    ✨ feat(entities): 新增role角色表

---
+++ src/enums/enum.role.ts
@@ -0,0 +1,19 @@
+/**角色类型**/
+export enum RoleType {
+    /**管理员**/
+    admin = 'admin',
+    /**部门角色**/
+    depart = 'depart',
+    /**基础角色**/
+    basic = 'basic',
+    /**通用角色**/
+    general = 'general'
+}
+
+/**角色状态**/
+export enum RoleState {
+    /**启用**/
+    enable = 'enable',
+    /**禁用**/
+    disable = 'disable'
+}
