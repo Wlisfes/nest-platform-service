92aa690ddfc9906da362e85a0a864ae19a977253
妖雨纯 <876451336@qq.com>
2025-04-22 12:34:44 +0800

    ✨ feat(config): 修改接口文档优先级服务

--- src/config/web-document.ts
+++ src/config/web-document.ts
@@ -2,18 +2,18 @@ import { omit } from 'lodash'
 
 export const documentOptions = [
     {
-        name: 'web-common-service',
-        swaggerVersion: '1.0.0',
-        prefix: '/api/common',
-        baseUrl: `http://localhost:3010`,
-        url: `/api/swagger-json`,
-        location: `/api/swagger`
-    },
-    {
         name: 'web-system-service',
         swaggerVersion: '1.0.0',
         prefix: '/api/system',
         baseUrl: `http://localhost:3020`,
+        url: `/api/swagger-json`,
+        location: `/api/swagger`
+    },
+    {
+        name: 'web-common-service',
+        swaggerVersion: '1.0.0',
+        prefix: '/api/common',
+        baseUrl: `http://localhost:3010`,
         url: `/api/swagger-json`,
         location: `/api/swagger`
     }
